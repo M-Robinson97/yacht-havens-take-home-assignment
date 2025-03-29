3e30cc1062163ccdd8bd39513fecc25add73af51
Mark Robinson <m-robinson97@outlook.com>
2025-03-29 16:46:52 +0700

    Form calculation WIP - getting thrown out of cafe!

--- take-home-assignment/src/app/components/edit-contract-dialog/edit-contract-dialog.component.ts
+++ take-home-assignment/src/app/components/edit-contract-dialog/edit-contract-dialog.component.ts
@@ -8,6 +8,7 @@ import { ContractType } from 'src/app/enums/contract-type.enum';
 import { EnumView } from 'src/app/models/enum-view.model';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { EnumService } from 'src/app/services/enum.service';
+import { GetQuoteParams } from 'src/app/models/quote.model';
 
 @Component({
     selector: 'app-edit-contract-dialog',
@@ -16,6 +17,7 @@ import { EnumService } from 'src/app/services/enum.service';
 })
 export class EditContractDialogComponent implements OnInit {
 
+    public quote!: number;
     public statuses$!: Observable<EnumView<ContractStatus>[]>;
     public contractTypes$!: Observable<EnumView<ContractType>[]>;
     public startDate: string = '';
@@ -35,6 +37,7 @@ export class EditContractDialogComponent implements OnInit {
     ngOnInit(): void {
         this.statuses$ = this.enumService.getContractStatuses();
         this.contractTypes$ = this.enumService.getContractTypes();
+        this.quote = this.data.contract.totalIncVat;
         this.addControls();
     }
 
@@ -46,10 +49,34 @@ export class EditContractDialogComponent implements OnInit {
     }
 
     private getQuote(): void {
-        throw new Error('Method not implemented.');
+        const quoteParams = this.buildQuoteParams();
+        this.quoteService.getQuote(quoteParams).subscribe({
+            next: (value) => {
+              this.quote = value;
+              console.log(this.quote);
+            },
+            error: (err) => {
+              console.error('Error fetching quote:', err);
+            }
+          });
+    }
+
+    private buildQuoteParams(): GetQuoteParams{
+        return {
+            contractType: this.form.get('contractType')?.value ?? this.data.contract.contractType,
+            startDate: this.form.get('startDate')?.value ?? this.data.contract.startDate,
+            endDate: this.form.get('endDate')?.value ?? this.data.contract.endDate,
+        };
     }
 
     public update(): void {
-        throw new Error('Method not implemented.');
+        this.getQuote();
+
+        /*
+        NOTE: Quote has been successfully calculated and stored as getQuote
+        NEED TO:
+        - DISPLAY QUOTE ON SCREEN WHEN UPDATING FIELD RATHER THAN ON CLICKING UPDATE
+        - CLICKING UPDATE SHOULD UPDATE RELEVANT ROW
+        */
     }
 }
