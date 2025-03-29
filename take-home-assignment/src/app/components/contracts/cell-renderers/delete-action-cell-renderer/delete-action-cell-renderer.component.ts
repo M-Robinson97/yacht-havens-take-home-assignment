c4d43a507feec691c6e6ad53e73bbcec642cc722
Mark Robinson <m-robinson97@outlook.com>
2025-03-29 10:54:45 +0700

    Add logic to delete a contract

--- take-home-assignment/src/app/components/contracts/cell-renderers/delete-action-cell-renderer/delete-action-cell-renderer.component.ts
+++ take-home-assignment/src/app/components/contracts/cell-renderers/delete-action-cell-renderer/delete-action-cell-renderer.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ICellRendererAngularComp } from 'ag-grid-angular';
-import { ICellRendererParams, IAfterGuiAttachedParams } from 'ag-grid-community';
+import { ICellRendererParams } from 'ag-grid-community';
+import { ContractsService } from 'src/app/services/contracts.service';
 
 @Component({
   selector: 'app-delete-action-cell-renderer',
@@ -8,14 +9,35 @@ import { ICellRendererParams, IAfterGuiAttachedParams } from 'ag-grid-community'
   styleUrls: ['./delete-action-cell-renderer.component.scss']
 })
 export class DeleteActionCellRendererComponent implements ICellRendererAngularComp {
+  params!: ICellRendererParams;
 
-  constructor() { }
-  refresh(params: any): boolean {
-    console.log('refresh delete cell renderer');
+  constructor(private contractsService: ContractsService) { }
+
+  agInit(params: ICellRendererParams): void {
+    this.params = params;
+  }
+
+  refresh(params: ICellRendererParams ): boolean {
+    // This method is not currently needed
     return true;
   }
-  
-  agInit(params: ICellRendererParams): void {
-    console.log('init delete cell renderer');
+
+  deleteContract() {
+    const gridApi = this.params.api;
+    const rowNode = this.params.node;
+
+    // TODO: Upgrade 'confirm' dialogue to modal
+    if (confirm(`Are you sure you want to delete the contract for ${this.params.data.customerName}?`)) {
+      this.contractsService.deleteContract(this.params.data.id).subscribe({
+        next: () => {
+          alert('Contract deleted');
+          gridApi.removeItems([rowNode]);
+        },
+        error: (err) => {
+          alert('There was a problem deleting the contract');
+          console.error(err);
+        }
+      });
+    }
   }
 }
