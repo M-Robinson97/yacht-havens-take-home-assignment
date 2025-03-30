3d63477f2d1f698924e0a0c00e05b6fbcce3a0d3
Mark Robinson <m-robinson97@outlook.com>
2025-03-30 18:01:46 +0700

    Quote recalculation WIP

--- take-home-assignment/src/app/components/edit-contract-dialog/edit-contract-dialog.component.ts
+++ take-home-assignment/src/app/components/edit-contract-dialog/edit-contract-dialog.component.ts
@@ -1,12 +1,12 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { EditContractDialogData } from './edit-contract-dialog.component.model';
 import { QuoteService } from 'src/app/services/quote.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { ContractStatus } from 'src/app/enums/contract-status.enum';
 import { ContractType } from 'src/app/enums/contract-type.enum';
 import { EnumView } from 'src/app/models/enum-view.model';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { EnumService } from 'src/app/services/enum.service';
 import { GetQuoteParams } from 'src/app/models/quote.model';
 
@@ -15,15 +15,17 @@ import { GetQuoteParams } from 'src/app/models/quote.model';
     templateUrl: './edit-contract-dialog.component.html',
     styleUrls: ['./edit-contract-dialog.component.scss'],
 })
-export class EditContractDialogComponent implements OnInit {
+export class EditContractDialogComponent implements OnInit, OnDestroy {
 
-    public quote!: number;
+    public contractId!: number;
+    public customerName!: string;
     public statuses$!: Observable<EnumView<ContractStatus>[]>;
     public contractTypes$!: Observable<EnumView<ContractType>[]>;
-    public startDate: string = '';
-    public endDate: string = '';
-
     public form: FormGroup = this.fb.group({});
+
+    private contractTypeSubscription: Subscription | undefined;
+    private startDateSubscription: Subscription | undefined;
+    private endDateSubscription: Subscription | undefined;
     
     constructor(
         @Inject(MAT_DIALOG_DATA)
@@ -35,25 +37,47 @@ export class EditContractDialogComponent implements OnInit {
     ) {}
 
     ngOnInit(): void {
+        this.contractId = this.data.contract.id;
+        this.customerName = this.data.contract.customerName;
         this.statuses$ = this.enumService.getContractStatuses();
         this.contractTypes$ = this.enumService.getContractTypes();
-        this.quote = this.data.contract.totalIncVat;
-        this.addControls();
+        this.buildForm();
+        this.subscribeToFormChanges();
     }
 
-    private addControls(): void {
-        this.form.addControl('status', this.fb.control(null));
-        this.form.addControl('contractType', this.fb.control(null));
-        this.form.addControl('startDate', this.fb.control(null));
-        this.form.addControl('endDate', this.fb.control(null));
+    private buildForm(): void {
+        //const formattedQuote = this.quoteService.formatQuoteWithCurrency(this.data.contract.currency, this.quote);
+        //console.log(formattedQuote);
+        this.form.addControl('status', this.fb.control(this.data.contract.contractStatus));
+        this.form.addControl('contractType', this.fb.control(this.data.contract.contractType));
+        this.form.addControl('startDate', this.fb.control(this.data.contract.startDate));
+        this.form.addControl('endDate', this.fb.control(this.data.contract.endDate));
+        this.form.addControl('quote', this.fb.control(this.data.contract.totalIncVat));
+    }
+
+    private subscribeToFormChanges() {
+        this.contractTypeSubscription = (this.form.get('contractType') as FormControl).valueChanges.subscribe(value => {
+            this.recalculateQuote();
+        });
+        this.startDateSubscription = (this.form.get('startDate') as FormControl).valueChanges.subscribe(value => {
+            this.recalculateQuote();
+        });
+        this.endDateSubscription = (this.form.get('endDate') as FormControl).valueChanges.subscribe(value => {
+            this.recalculateQuote();
+        });
+    }
+
+    private recalculateQuote() {
+        this.getQuote();
+        
     }
 
     private getQuote(): void {
         const quoteParams = this.buildQuoteParams();
         this.quoteService.getQuote(quoteParams).subscribe({
-            next: (value) => {
-              this.quote = value;
-              console.log(this.quote);
+            next: (newQuote) => {
+              (this.form.get('startDate') as FormControl)?.patchValue(newQuote);
+              console.log(newQuote);
             },
             error: (err) => {
               console.error('Error fetching quote:', err);
@@ -78,5 +102,11 @@ export class EditContractDialogComponent implements OnInit {
         - DISPLAY QUOTE ON SCREEN WHEN UPDATING FIELD RATHER THAN ON CLICKING UPDATE
         - CLICKING UPDATE SHOULD UPDATE RELEVANT ROW
         */
+    }
+
+    public ngOnDestroy() {
+        if (this.contractTypeSubscription) this.contractTypeSubscription.unsubscribe();
+        if (this.startDateSubscription) this.startDateSubscription.unsubscribe();
+        if (this.endDateSubscription) this.endDateSubscription.unsubscribe();
     }
 }
