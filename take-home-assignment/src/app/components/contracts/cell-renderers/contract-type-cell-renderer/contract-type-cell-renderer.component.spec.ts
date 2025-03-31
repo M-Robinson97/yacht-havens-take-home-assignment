51b06088d655c68f099f1952c7dfbb59d98ba0ae
Mark Robinson <m-robinson97@outlook.com>
2025-03-31 11:17:13 +0700

    Cell renderers - whitespace and unused imports

--- take-home-assignment/src/app/components/contracts/cell-renderers/contract-type-cell-renderer/contract-type-cell-renderer.component.spec.ts
+++ take-home-assignment/src/app/components/contracts/cell-renderers/contract-type-cell-renderer/contract-type-cell-renderer.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ContractTypeCellRendererComponent } from './contract-type-cell-renderer.component';
 
 describe('ContractTypeCellRendererComponent', () => {
